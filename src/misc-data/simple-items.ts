b4d04c4ac5dbae573916f6433d54a0ac603710ac
karapeev <karapeev@progress.com>
2022-03-29 17:09:39 +0300

    refactor: use countries instead of items

--- src/misc-data/simple-items.ts
+++
@@ -1,7 +0,0 @@
-export function getItems(numberOfItems: number, itemName: string = 'Item'): string[] {
-    return Array(numberOfItems + 1).fill(itemName + ' ').map((item, index) => item + index).slice(1, numberOfItems + 1);
-}
-
-export function getItemsZeroBased(numberOfItems: number, itemName: string = 'Item'): string[] {
-    return Array(numberOfItems).fill(itemName + ' ').map((item, index) => item + index);
-}
