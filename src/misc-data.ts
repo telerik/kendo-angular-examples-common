283b38af48c6525e3fe941c29ee42b7da8982021
Georgi K <45083324+gkarapeev@users.noreply.github.com>
2022-03-30 17:39:49 +0300

    feat: add simple items (#11)

--- src/misc-data.ts
+++ src/misc-data.ts
@@ -3,3 +3,4 @@ export * from './misc-data/task.interface';
 export * from './misc-data/filesystem';
 export * from './misc-data/tasks';
 export * from './misc-data/flat-tasks';
+export * from './misc-data/countries';
