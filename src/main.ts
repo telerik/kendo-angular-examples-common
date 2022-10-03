49578ea594c219a30153a2f17a44c8157dc22039
Tsvetomir Tsonev <cvetomirconev@gmail.com>
2022-10-03 15:14:10 +0300

    feat: add CSP and routing services

--- src/main.ts
+++ src/main.ts
@@ -1,3 +1,5 @@
 export * from './employees';
 export * from './products';
 export * from './misc-data';
+export { CSPConfig, CSPService } from './services/csp-service';
+export { PersistUrlOnErrorService } from './services/persist-url-on-error.service';
